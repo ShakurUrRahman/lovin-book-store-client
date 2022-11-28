09b45d39fd2602eaa18f396eb5131cb2336a0398
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-11-28 22:06:58 +0600

    added reamme file

--- src/Hooks/useSeller.js
+++ src/Hooks/useSeller.js
@@ -5,7 +5,7 @@ const useSeller = email => {
     const [isSellerLoading, setIsSellerLoading] = useState(true);
     useEffect(() => {
         if (email) {
-            fetch(`https://lovin-book-store-server.vercel.app/buyers/sellers/${email}`)
+            fetch(`https://lovin-book-store-server.vercel.app/buyers/seller/${email}`)
                 .then(res => res.json())
                 .then(data => {
                     console.log(data);
